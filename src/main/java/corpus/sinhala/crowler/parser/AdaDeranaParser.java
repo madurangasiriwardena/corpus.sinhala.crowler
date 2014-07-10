56b9fddc20e0044c66aea1d739293734b80a6d20
cdwijayarathna <cdwijayarathna@gmail.com>
2014-07-10 16:28:01 +0530

    AdaDerana Parser Added

---
+++ src/main/java/corpus/sinhala/crowler/parser/AdaDeranaParser.java
@@ -0,0 +1,83 @@
+package corpus.sinhala.crowler.parser;
+
+import org.jsoup.Jsoup;
+import org.jsoup.nodes.Document;
+import org.jsoup.nodes.Element;
+import org.jsoup.select.Elements;
+
+public class AdaDeranaParser implements Parser{
+	Document doc;
+	String url;
+	Element titleElement;
+	Element p;
+	String[] arr;
+
+	public AdaDeranaParser(String page, String url){
+		doc = Jsoup.parse(page);
+		this.url = url;
+//		titleElement = doc.select("h2").first();
+//		if(titleElement.select("img").size()==0){
+//			titleElement = doc.select("h2").first();
+//		}else{
+//			titleElement = doc.select("h2").get(1);
+//		}
+//		p = titleElement.parent();
+//		arr = url.split("/");
+
+	}
+
+	public String getTitle(){
+		return doc.select("h2[class=completeNewsTitle]").first().text();
+	}
+
+	public String getAuthor(){
+		return "";
+	}
+
+	public String getContent(){
+		return doc.select("div[class=newsContent]").first().text() ;
+	}
+
+	public String getUrl(){
+		return url;
+	}
+
+	public String getYear(){
+		return doc.select("p[class=newsDateStamp]").first().text().split(" ")[2].substring(0, 4);
+	}
+
+	public String getMonth(){
+		String month = doc.select("p[class=newsDateStamp]").first().text().split(" ")[0];
+		if(month.equals("January")){
+			return "01";
+		}else if(month.equals("February")){
+			return "02";
+		}else if(month.equals("March")){
+			return "03";
+		}else if(month.equals("April")){
+			return "04";
+		}else if(month.equals("May")){
+			return "05";
+		}else if(month.equals("June")){
+			return "06";
+		}else if(month.equals("July")){
+			return "07";
+		}else if(month.equals("August")){
+			return "08";
+		}else if(month.equals("September")){
+			return "09";
+		}else if(month.equals("October")){
+			return "10";
+		}else if(month.equals("November")){
+			return "11";
+		}else if(month.equals("December")){
+			return "12";
+		}
+		return "";
+	}
+
+	public String getDate(){
+		return doc.select("p[class=newsDateStamp]").first().text().split(" ")[1].substring(0,2);
+	}
+
+}
