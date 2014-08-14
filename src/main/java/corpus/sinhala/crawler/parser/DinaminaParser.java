dfd730abb1b679e9a8a86732b5ece95dba2741bf
madurangasiriwardena <maduranga.siriwardena@gmail.com>
2014-08-14 16:03:23 +0530

    renaming the packages

---
+++ src/main/java/corpus/sinhala/crawler/parser/DinaminaParser.java
@@ -0,0 +1,68 @@
+package corpus.sinhala.crawler.parser;
+
+import org.jsoup.Jsoup;
+import org.jsoup.nodes.Document;
+import org.jsoup.nodes.Element;
+import org.jsoup.select.Elements;
+
+public class DinaminaParser implements Parser{
+	Document doc;
+	String url;
+	Element titleElement;
+	Element p;
+	String[] arr;
+
+	public DinaminaParser(String page, String url){
+		doc = Jsoup.parse(page);
+		this.url = url;
+		titleElement = doc.select("h2").first();
+		p = titleElement.parent();
+		arr = url.split("/");
+
+	}
+
+	public String getTitle(){
+		return titleElement.text();
+	}
+
+	public String getAuthor(){
+		String author = doc.select("p[class=byline]").first().text();
+		if(url.contains("samakaya")){
+		author = doc.select("p[class=byline]").get(1).text();
+		}
+		if(author == null){
+			author = "";
+		}
+		return author;
+	}
+
+	public String getContent(){
+		return p.text();
+	}
+
+	public String getUrl(){
+		return url;
+	}
+
+	public String getYear(){
+		if(arr.length==7)
+			return arr[3];
+		else
+			return "";
+	}
+
+	public String getMonth(){
+		if(arr.length==7)
+			return arr[4];
+		else
+			return "";
+	}
+
+	public String getDate(){
+		if(arr.length==7)
+			return arr[5];
+		else
+			return "";
+	}
+
+}
---
+++ src/main/java/corpus/sinhala/crawler/url/generator/Controller.java
@@ -0,0 +1,53 @@
+package corpus.sinhala.crawler.url.generator;
+
+import java.io.IOException;
+
+import javax.xml.stream.XMLStreamException;
+
+import org.jsoup.nodes.Document;
+
+import corpus.sinhala.crawler.XMLFileWriter;
+
+public class Controller {
+	public static void main(String args[]) throws IOException, XMLStreamException{
+		String startDate;
+		String endDate;
+		String host;
+		int port;
+		String saveLocation;
+		if(args.length == 5){
+			startDate = args[0];
+			endDate = args[1];
+			host = args[2];
+			port = Integer.parseInt(args[3]);
+			saveLocation = args[4];
+		}else{
+			return;
+		}
+		
+		String temp1[] = startDate.split("/");
+		String temp2[] = endDate.split("/");
+		int sYear = Integer.parseInt(temp1[0]);
+		int eYear = Integer.parseInt(temp2[0]);
+		int sMonth = Integer.parseInt(temp1[1]);
+		int eMonth = Integer.parseInt(temp2[1]);
+		int sDate = Integer.parseInt(temp1[2]);
+		int eDate = Integer.parseInt(temp2[2]);
+//		DivainaGenerator dg = new DivainaGenerator(2010, 2010, 1, 1, 1, 4, "127.0.0.1", 12345);
+		DivainaGenerator dg = new DivainaGenerator(sYear, eYear, sMonth, eMonth, sDate, eDate, host, port);
+		XMLFileWriter xfw = new XMLFileWriter(saveLocation);
+		dg.addObserver(xfw);
+
+		Document doc;
+		while((doc = dg.fetchPage()) != null){
+			System.out.println(doc.baseUri());
+			try{
+				xfw.addDocument(doc.html(), doc.baseUri());
+			}catch(Exception e){
+				
+			}
+			
+		}
+
+	}
+}
