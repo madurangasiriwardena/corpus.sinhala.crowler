9881a1ce6443b8ca8237427c98876ddc3de47466
madurangasiriwardena <maduranga.siriwardena@gmail.com>
2014-09-11 14:35:44 +0530

    AdaDerana crawler completed

---
+++ crawlers/corpus.sinhala.crawler.adaderana/src/main/java/corpus/sinhala/crawler/AdaDeranaGenerator.java
@@ -0,0 +1,198 @@
+package corpus.sinhala.crawler;
+
+import java.io.BufferedReader;
+import java.io.DataOutputStream;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.net.HttpURLConnection;
+import java.net.InetSocketAddress;
+import java.net.Proxy;
+import java.net.URL;
+import java.util.ArrayList;
+import java.util.LinkedList;
+import java.util.List;
+import java.util.Queue;
+
+import org.apache.http.HttpEntity;
+import org.apache.http.HttpResponse;
+import org.apache.http.NameValuePair;
+import org.apache.http.client.HttpClient;
+import org.apache.http.client.entity.UrlEncodedFormEntity;
+import org.apache.http.client.methods.HttpPost;
+import org.apache.http.impl.client.HttpClients;
+import org.apache.http.message.BasicNameValuePair;
+import org.joda.time.DateTime;
+import org.joda.time.DateTimeConstants;
+import org.jsoup.Jsoup;
+import org.jsoup.nodes.Document;
+import org.jsoup.select.Elements;
+
+import corpus.sinhala.crawler.infra.Generator;
+import corpus.sinhala.crawler.infra.network.NetworkConnector;
+
+public class AdaDeranaGenerator extends Generator {
+	int sYear;
+	int eYear;
+	int sMonth;
+	int eMonth;
+	int sDate;
+	int eDate;
+
+	int year;
+	int month;
+	int date;
+	int articleId;
+	String[] articleName;
+	int articleNameId;
+
+	DateTime dt;
+	DateTime endDate;
+	DateTime dayAfterEndDate;
+	boolean startDate;
+
+	boolean listEmpty;
+	Queue<String> urls;
+
+	NetworkConnector nc;
+
+	public AdaDeranaGenerator(int sYear, int eYear, int sMonth, int eMonth,
+			int sDate, int eDate, String host, int port) {
+
+		this.sYear = sYear;
+		this.eYear = eYear;
+		this.sMonth = sMonth;
+		this.eMonth = eMonth;
+		this.sDate = sDate;
+		this.eDate = eDate;
+		startDate = true;
+
+		year = sYear;
+		month = sMonth;
+		date = sDate;
+
+
+		urls = new LinkedList<String>();
+
+		dt = new DateTime(sYear, sMonth, sDate, 0, 0, 0, 0);
+
+		year = dt.getYear();
+		month = dt.getMonthOfYear();
+		date = dt.getDayOfMonth();
+		System.out.println("Crawling from " + dt);
+		endDate = new DateTime(eYear, eMonth, eDate, 0, 0, 0, 0);
+		System.out.println("To " + endDate);
+		dayAfterEndDate = endDate.plusDays(1);
+
+		nc = NetworkConnector.getInstance();
+		try {
+			nc.connect(host, port);
+		} catch (IOException e) {
+			e.printStackTrace();
+		}
+	}
+
+
+	public Document fetchPage() throws IOException {
+		if (!dt.isBefore(dayAfterEndDate)) {
+			return null;
+		}
+		while (urls.isEmpty()) {
+			if(!startDate){
+				try {
+					String message = year + "-" + String.format("%02d", month)
+							+ "-" + String.format("%02d", date);
+					nc.send(year + "/" + String.format("%02d", month) + "/"
+							+ String.format("%02d", date));
+					setChanged();
+					notifyObservers(message);
+				} catch (IOException e1) {
+					return null;
+				}
+				articleNameId = 0;
+				dt = dt.plusDays(1);
+				year = dt.getYear();
+				month = dt.getMonthOfYear();
+				date = dt.getDayOfMonth();
+
+				if (!dt.isBefore(dayAfterEndDate)) {
+					try {
+						nc.send("close");
+						nc.close();
+					} catch (IOException e1) {
+						return null;
+					}
+
+					return null;
+				}
+
+			}else{
+				startDate = false;
+			}
+
+			HttpPost post = new HttpPost("http://sinhala.adaderana.lk/news_archive.php?srcRslt=1");
+	    	List<NameValuePair> params = new ArrayList<NameValuePair>(2);
+	    	params.add(new BasicNameValuePair("srcCategory", "999"));
+	    	params.add(new BasicNameValuePair("srcYear", year+""));
+	    	params.add(new BasicNameValuePair("srcMonth", month+""));
+	    	params.add(new BasicNameValuePair("srcDay", date+""));
+	    	params.add(new BasicNameValuePair("Submit", "Search"));
+			post.setEntity(new UrlEncodedFormEntity(params, "UTF-8"));
+
+			HttpClient httpclient = HttpClients.createDefault();
+			HttpResponse response = httpclient.execute(post);
+			HttpEntity entity = response.getEntity();
+
+			if (entity != null) {
+				InputStream instream = entity.getContent();
+
+				String line = null;
+				StringBuffer tmp = new StringBuffer();
+				BufferedReader in = new BufferedReader(new InputStreamReader(
+						instream, "UTF-8"));
+				while ((line = in.readLine()) != null) {
+					tmp.append(line);
+				}
+				Document doc = Jsoup.parse(String.valueOf(tmp));
+				Elements urlList = doc.select("div[class=story-text]");
+				for(int i=0; i<urlList.size(); i++){
+					
+					String tempUrl = urlList.get(i).select("h4").get(0).select("a").get(0).attr("href");
+					tempUrl = "http://sinhala.adaderana.lk/"+tempUrl;
+					if( !urls.contains(tempUrl))
+					urls.add(tempUrl);
+				}
+			}
+
+			
+			System.out.println(urls.isEmpty());
+		}
+
+		String urlString = urls.remove();
+		URL url = new URL(urlString);
+		Proxy proxy = new Proxy(Proxy.Type.HTTP, new InetSocketAddress(
+				"cache.mrt.ac.lk", 3128));
+		// HttpURLConnection uc = (HttpURLConnection) url.openConnection(proxy);
+		HttpURLConnection uc = (HttpURLConnection) url.openConnection();
+
+		try {
+			uc.connect();
+			String line = null;
+			StringBuffer tmp = new StringBuffer();
+			BufferedReader in = new BufferedReader(new InputStreamReader(
+					uc.getInputStream(), "UTF-8"));
+			while ((line = in.readLine()) != null) {
+				tmp.append(line);
+			}
+
+			Document doc = Jsoup.parse(String.valueOf(tmp));
+			doc.setBaseUri(urlString);
+			return doc;
+
+		} catch (IOException e) {
+
+		}
+		return null;
+	}
+
+}
---
+++ crawlers/corpus.sinhala.crawler.adaderana/src/main/java/corpus/sinhala/crawler/AdaderanaParser.java
@@ -0,0 +1,71 @@
+package corpus.sinhala.crawler;
+
+import org.jsoup.Jsoup;
+import org.jsoup.nodes.Document;
+
+import corpus.sinhala.crawler.infra.Parser;
+
+public class AdaderanaParser implements Parser{
+	Document doc;
+	String url;
+
+	public AdaderanaParser(String page, String url){
+		doc = Jsoup.parse(page);
+		this.url = url;
+	}
+
+	public String getTitle(){
+		return doc.select("h2[class=completeNewsTitle]").first().text();
+	}
+
+	public String getAuthor(){
+		return "";
+	}
+
+	public String getContent(){
+		return doc.select("div[class=newsContent]").first().text() ;
+	}
+
+	public String getUrl(){
+		return url;
+	}
+
+	public String getYear(){
+		return doc.select("p[class=newsDateStamp]").first().text().split(" ")[2].substring(0, 4);
+	}
+
+	public String getMonth(){
+		String month = doc.select("p[class=newsDateStamp]").first().text().split(" ")[0];
+		if(month.equals("January")){
+			return "01";
+		}else if(month.equals("February")){
+			return "02";
+		}else if(month.equals("March")){
+			return "03";
+		}else if(month.equals("April")){
+			return "04";
+		}else if(month.equals("May")){
+			return "05";
+		}else if(month.equals("June")){
+			return "06";
+		}else if(month.equals("July")){
+			return "07";
+		}else if(month.equals("August")){
+			return "08";
+		}else if(month.equals("September")){
+			return "09";
+		}else if(month.equals("October")){
+			return "10";
+		}else if(month.equals("November")){
+			return "11";
+		}else if(month.equals("December")){
+			return "12";
+		}
+		return "";
+	}
+
+	public String getDate(){
+		return doc.select("p[class=newsDateStamp]").first().text().split(" ")[1].substring(0,2);
+	}
+
+}
