f9b3416022ba22fddc5db5c5f3a921163a773d73
cdwijayarathna <cdwijayarathna@gmail.com>
2015-01-08 20:03:01 +0530

    Subtitle Crawler Finished

--- crawlers/corpus.sinhala.crawler.subtitles/src/main/java/corpus/sinhala/crawler/subtitles/Crawler.java
+++ crawlers/corpus.sinhala.crawler.subtitles/src/main/java/corpus/sinhala/crawler/subtitles/Crawler.java
@@ -6,6 +6,12 @@ import java.io.IOException;
 import java.io.InputStream;
 import java.io.InputStreamReader;
 import java.lang.reflect.InvocationTargetException;
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.sql.Statement;
 
 import javax.xml.stream.XMLStreamException;
 
@@ -23,6 +29,46 @@ import org.jsoup.select.Elements;
 
 public class Crawler {
     int counter = 0;
+    Connection conn = null;
+	Statement stmt = null;
+	ResultSet rs = null;
+	String driverName;
+	String url;
+	String uname;
+	String pwd;
+	
+	public Crawler() {
+		driverName = "com.mysql.jdbc.Driver";
+		url = "jdbc:mysql://"+"localhost"+":3306/crawler_data";
+		uname = "root";
+		pwd = "";
+		if (conn == null)
+			conn = getConnection(driverName, url, "root", "");
+	}
+	
+	public Connection getConnection(String driver, String url, String uname,
+			String pwd) {
+		Connection conn = null;
+
+		try {
+			Class.forName(driver).newInstance();
+			conn = DriverManager.getConnection(url, uname, pwd);
+
+		} catch (SQLException ex) {
+			ex.printStackTrace();
+		} catch (InstantiationException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		} catch (IllegalAccessException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		} catch (ClassNotFoundException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		} 
+
+		return conn;
+	}
 	public static void main(String[] args) throws ClientProtocolException, IOException, InstantiationException, IllegalAccessException, IllegalArgumentException, InvocationTargetException, XMLStreamException, ClassNotFoundException, NoSuchMethodException, SecurityException {
 		Crawler c = new Crawler();
 		Parser p = new Parser();
@@ -53,23 +99,30 @@ public class Crawler {
 
 				
 				Document doc = Jsoup.parse(String.valueOf(tmp));
-
+				if(doc.select("title").first().text().contains("Page not found")){
+					break;
+				}
 				Elements urlList = doc.select("article[class=item-list]");
 				for(int i=0; i<urlList.size(); i++){
 					
 					String tempUrl = urlList.get(i).select("a").first().attr("href");
-					if(false){
-						//Database check if already craawled
+					if(checkDB(tempUrl)){
+						writer.update();
 						break outer;
 					}
-					getZip(tempUrl,d,SaveLoc, writer,p);
-					System.out.println(tempUrl);
+					try{
+						getZip(tempUrl,d,SaveLoc, writer,p);
+						writeDB(tempUrl);
+					}catch(Exception e){
+						System.out.println(tempUrl);
+					}
 				}
 	    	}
 	    	count++;
-	    	break;
+	    	//break;
 	    	
 		}
+		//System.out.println("break");
 	}
 	
 	public void getZip(String url, Downloader d, String saveLoc, XMLFileWriter writer, Parser p) throws ClientProtocolException, IOException, InstantiationException, IllegalAccessException, IllegalArgumentException, InvocationTargetException, XMLStreamException{
@@ -80,6 +133,7 @@ public class Crawler {
     	HttpEntity entity = response.getEntity();
     	
     	if (entity != null) {
+    		System.out.println("True");
     	    InputStream instream = entity.getContent();
     	    
     	    String line = null;
@@ -94,7 +148,7 @@ public class Crawler {
 
 			Elements zipUrl = doc.select("a");
 			for(int i=0;i<zipUrl.size();i++){
-				if(zipUrl.get(i).text().equals("සිංහල උපසිරැසි මෙතනින් බාගන්න")){
+				if(zipUrl.get(i).text().contains("සිංහල උපසිරැසි මෙතනින් බාගන්න")){
 					String link = zipUrl.get(i).attr("href");
 					//d.Download(link, saveLoc );
 					System.out.println(link);
@@ -128,6 +182,40 @@ public class Crawler {
 			}
 			
     	}
+	}
+	
+	boolean checkDB(String url){
+		String query = "SELECT * FROM subtitle_content WHERE url = ?";
+		ResultSet rs;
+
+		PreparedStatement stmt3;
+		try {
+			stmt3 = conn.prepareStatement(query);
+			stmt3.setString(1, url);
+			rs = stmt3.executeQuery();
+			if (rs.first()) {
+				return true;
+			}
+			return false;
+		} catch (SQLException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+		return false;
+	}
+	
+	public void writeDB(String url){
+		String query = "INSERT INTO subtitle_content (`url`) VALUES (?);";
+
+		PreparedStatement stmt3;
+		try {
+			stmt3 = conn.prepareStatement(query);
+			stmt3.setString(1, url);
+			stmt3.execute();
+		} catch (SQLException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
 	}
 	
 	
